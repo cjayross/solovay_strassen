b738f44733300f8034b3953b6b300e28aabcf21c
cjayross <calvinjayross@gmail.com>
2019-12-10 01:56:55 -0600

    converted to a library

--- src/prime.rs
+++ src/prime.rs
@@ -1,13 +1,9 @@
 extern crate rand;
 extern crate rayon;
-extern crate clap;
 
 use std::{u64, mem};
-use clap::{Arg, App};
 use rand::distributions::{Distribution, Uniform};
 use rayon::prelude::*;
-
-const K: usize = 100;
 
 fn mod_pow(mut x: u64, mut exp: u64, n: u64) -> u64 {
   let mut res = 1;
@@ -51,7 +47,7 @@ fn legendre_symbol(mut a: u64, mut n: u64) -> i64 {
   }
 }
 
-fn solovay_strassen(a: u64, n: u64) -> bool {
+pub fn solovay_strassen(a: u64, n: u64) -> bool {
   let x: i64 = legendre_symbol(a, n);
 
   if x == 0 || x.rem_euclid(n as i64) as u64 != mod_pow(a, (n - 1) / 2, n) {
@@ -62,7 +58,7 @@ fn solovay_strassen(a: u64, n: u64) -> bool {
   }
 }
 
-fn is_prime(n: u64, k: usize) -> bool {
+pub fn is_prime(n: u64, k: usize) -> bool {
   let dist = Uniform::new(2, n);
   let rng = rand::thread_rng();
   let samples: Vec<u64> = dist.sample_iter(rng).take(k).collect();
@@ -70,38 +66,10 @@ fn is_prime(n: u64, k: usize) -> bool {
   !samples.par_iter().find_any(|&&a| solovay_strassen(a, n)).is_some()
 }
 
-fn main() {
-  let matches = App::new("Solovay-Strassen Test")
-    .version("0.1.0")
-    .author("cjayross <calvinjayross@gmail.com>")
-    .about("Simple Solovay-Strassen algorithm for testing the primality of integers.")
-    .arg(Arg::with_name("integer")
-      .short("n")
-      .long("integer")
-      .takes_value(true)
-      .help("Integer to test"))
-    .get_matches();
-
-  let n;
-  let n_str = matches.value_of("integer");
-
-  match n_str {
-    None => panic!("Missing integer."),
-    Some(s) => {
-      match s.parse::<u64>() {
-        Ok(_n) => n = _n,
-        Err(_) => panic!("Invalid number, `{}`", s),
-      }
-    },
-  }
-
-  let result: bool = is_prime(n, K);
-
-  println!("{} {}", n, match result { false => "is composite", _ => "is likely prime" });
-}
-
 #[cfg(test)]
 mod tests {
+  const K: usize = 100;
+
   use super::*;
   use std::io;
 
